c017e0d134a478619b5038f90a8db91db39c52dc
admin <admin@DESKTOP-OKFSO5U>
2023-05-29 18:10:14 +0530

    Initial Commit

---
+++ java/src/string/ConvertString.java
@@ -0,0 +1,17 @@
+package string;
+
+public class ConvertString {
+	public static String convertString(String word) {
+		String s = "BAngaloreciTy";
+		char ch[] =s.toCharArray();
+		
+		for (int i=0; i<ch.length; i++) {
+			if(ch[i]>='A' && ch[i]<='Z')
+					ch[i]=(char) (ch[i]+32);
+			else if(ch[i]>='a' && ch[i]<='z')
+				ch[i]=(char) (ch[i]-32);
+			}
+	
+		return String.valueOf(ch);
+	}
+}
